93aca79c2bdfa554b75c57366a41c959c8d05759
Camilo Alarcon <camilo4750@gmail.com>
2025-02-18 16:43:51 -0500

    feat(auth): add login feature

---
+++ components/Navbar/Categories/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps({
+  option: String
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/transparent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col h-screen w-full overflow-hidden">
+    <div class="w-full flex justify-end p-4">
+      <UiBtnColorMode />
+    </div>
+    <div class="h-screen w-full overflow-y-auto overflow-x-hidden">
+    
+      <slot />
+    </div>
+  </div>
+</template>
