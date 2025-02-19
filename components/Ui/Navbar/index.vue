eea07b0aaa3f84e70d09c95698db3536391efdd5
Camilo Alarcon <camilo4750@gmail.com>
2025-02-19 10:48:54 -0500

    feat(navbar): add color mode button and dropdown for user and logout

--- components/Ui/Navbar/index.vue
+++ components/Ui/Navbar/index.vue
@@ -2,36 +2,61 @@
 const options = [
   {
     name: "Products",
-    link: "/"
+    link: "/",
   },
   {
     name: "Recipes",
-    link: "/"
+    link: "/",
   },
   {
     name: "Testimonials",
-    link: "/"
+    link: "/",
   },
   {
     name: "Post",
-    link: "/"
-  }
+    link: "/",
+  },
+];
+
+
+const itemsDrowdown = [
+  [{
+    label: useState("sessionUserName").value,
+    avatar: {
+      src: useState("sessionUserImg").value,
+    }
+  }],
+  [{
+    label: 'Logout',
+    icon: 'material-symbols:logout-rounded',
+    click: () => {
+      useAuth().logout()
+    }
+  }]
 ]
 </script>
 
 <template>
-  <div class="h-16 w-full bg-blue-300 flex justify-between items-center px-5 py-2">
-    <div class="">Dummy</div>
+  <div
+    class="h-16 w-full bg-gray-800 text-white flex justify-between items-center px-5 py-2"
+  >
+    <div class="font-bold text-2xl">Dummy</div>
     <div class="">
-      <ul class="flex gap-5 hover:font-bold">
-        <li class="hover:font-bold" v-for="option in options">{{ option.name }}</li>
+      <ul class="flex gap-5">
+        <li class="hover:font-bold cursor-pointer" v-for="option in options">{{ option.name }}</li>
       </ul>
     </div>
-    <div class="">
+    <div class="flex gap-3">
+      <UiBtnColorMode />
+      <UDropdown :items="itemsDrowdown" :popper="{ placement: 'bottom-start' }">
+        <UButton
+          color="white"
+          icon="material-symbols:person"
+          trailing-icon="i-heroicons-chevron-down-20-solid"
+        />
+      </UDropdown>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
