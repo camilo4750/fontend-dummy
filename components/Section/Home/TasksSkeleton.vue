df1615e698237accae5e2661d5d6e097efa71090
Camilo Alarcon <camilo4750@gmail.com>
2025-02-24 20:44:57 -0500

    fix(home): fix padding

--- components/Section/Home/TasksSkeleton.vue
+++ components/Section/Home/TasksSkeleton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-4 gap-5">
+  <div class="grid grid-cols-4 gap-5 p-5">
     <UCard class="animate-pulse h-32" v-for="index in 16" :key="index" />
   </div>
 </template>
